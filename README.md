2b70e67fe8e6960c984d26ae5f4a4edca7363607
prithivi-raj-varman <prithivi.kcet@gmail.com>
2025-12-25 11:52:19 +0530

    Hardcoded CAN frame data working successfully

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rzv2l_xlnxt_a54
+
+Source Files and folders added.
---
+++ metal/config.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015, Xilinx Inc. and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * @file	config.h
+ * @brief	Generated configuration settings for libmetal.
+ */
+
+#ifndef __METAL_CONFIG__H__
+#define __METAL_CONFIG__H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Library major version number. */
+#define METAL_VER_MAJOR		0
+
+/** Library minor version number. */
+#define METAL_VER_MINOR		1
+
+/** Library patch level. */
+#define METAL_VER_PATCH		0
+
+/** Library version string. */
+#define METAL_VER		"0.1.0"
+
+/** System type (linux, generic, ...). */
+#define METAL_SYSTEM		"linux"
+#define METAL_SYSTEM_LINUX
+
+/** Processor type (arm, x86_64, ...). */
+#define METAL_PROCESSOR		"aarch64"
+#define METAL_PROCESSOR_AARCH64
+
+/** Machine type (zynq, zynqmp, ...). */
+#define METAL_MACHINE		"rzv2l_r9a07g054l"
+#define METAL_MACHINE_RZV2L_R9A07G054L
+
+#define HAVE_STDATOMIC_H
+#define HAVE_FUTEX_H
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __METAL_CONFIG__H__ */
